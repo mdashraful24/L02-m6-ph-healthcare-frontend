2d83ed7f65554a42f3d43aa4d872bab148eff2b6
Ashraful Islam Ratul <mdashrafulislam2882@gmail.com>
2026-09-12 00:19:18 +0600

    added log in and logout successfully done

--- src/components/layout/public/Header.tsx
+++ src/components/layout/public/Header.tsx
@@ -1,11 +1,40 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { useGetMe, useLogout } from "@/hooks/auth.hook";
+import { toast } from "@/components/ui/toast";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Header() {
   const routes = [
     { name: "Home", url: "/" },
     { name: "About us", url: "/about-us" },
   ];
+
+  const { data, isLoading } = useGetMe();
+  const { mutate: logout } = useLogout();
+  const queryClient = useQueryClient();
+
+  const handleLogout = () => {
+    logout(undefined, {
+      onSuccess: () => {
+        toast.add({
+          title: "Logout Successful",
+          description: "You have been successfully logged out.",
+          type: "success",
+        });
+        queryClient.removeQueries({ queryKey: ["USER"] });
+      },
+      onError: (err) => {
+        toast.add({
+          title: "Logout Failed",
+          description: err.message || "Something went wrong. Please try again.",
+          type: "error",
+        });
+      },
+    });
+  };
 
   return (
     <header className="w-full h-16 border border-b">
@@ -21,13 +50,20 @@ export default function Header() {
         </nav>
 
         <div>
-          <Button
-            variant="outline"
-            render={<Link href="/login" />}
-            nativeButton={false}
-          >
-            Login
-          </Button>
+          {!isLoading && !data && (
+            <Button
+              variant="outline"
+              render={<Link href="/login" />}
+              nativeButton={false}
+            >
+              Login
+            </Button>
+          )}
+          {!isLoading && data && (
+            <Button variant="destructive" onClick={handleLogout}>
+              Logout
+            </Button>
+          )}
         </div>
       </div>
     </header>
