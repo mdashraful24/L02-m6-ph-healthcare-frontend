461ae39411f3e0d075a8e1a6999375137dcf51e4
Ashraful Islam Ratul <mdashrafulislam2882@gmail.com>
2026-09-11 19:58:51 +0600

    added project logo

--- src/app/(public)/(authentication)/login/page.tsx
+++ src/app/(public)/(authentication)/login/page.tsx
@@ -1,14 +1,15 @@
-import Link from "next/link";
 import LoginForm from "@/components/form/login-form";
+import Logo from "@/assets/svg/Logo";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <Link href="/" className="flex items-center gap-2 font-medium">
-            PH Healthcare
-          </Link>
+          <div className="flex items-center gap-2 text-xl font-bold">
+            <Logo />
+            <span>PH Healthcare</span>
+          </div>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
